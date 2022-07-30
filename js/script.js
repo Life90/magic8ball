f92b24e2c4abb844126a4540dc198bf77dbdc298
Life90 <68822929+Life90@users.noreply.github.com>
2022-07-29 21:50:44 -0700

    final magic 8 ball project

--- js/script.js
+++ js/script.js
@@ -23,22 +23,50 @@ document.querySelector("button").onclick = () => {
     "images/magic8ball_extra.png",
     "images/magic8ball_start.png"
   );
+
   let randomImg = Math.floor(Math.random() * magicBallsImg.length);
-  document.getElementById("magic").src = magicBallsImg[randomImg];
-  let ball = document.getElementById("magic");
-  ball.classList.add("shake");
+  let answers = document.getElementById("answers");
+  let fortune = document.createElement("img");
+  answers.appendChild(fortune);
+  fortune.id = "answer2";
+  document.getElementById("answer2").src = magicBallsImg[randomImg];
+
+  answers.classList.add("shake");
+
+  // ball.classList.add("rotate");
+
   setTimeout(function () {
-    ball.classList.remove("shake");
+    answers.classList.remove("shake");
   }, 1500);
-  // if (input.value.length <= 5) {
-  //   document.getElementById("test").disabled = true;
-  // }
+
+  // setTimeout(function () {
+  //   ball.classList.remove("rotate");
+  // }, 1500);
+
+  const input = document.querySelector("input").value;
+  let disabled = document.getElementById("test");
+  if (input.length <= 3) {
+    disabled = true;
+    alert("Hey you need to enter your question first");
+  } else if (input.length > 3) {
+    disabled = false;
+  }
 };
+
 function showImg() {
-  document.getElementById("congrats").src = "images/congrats.png";
-  let img = document.getElementById("congrats");
-  img.classList.add("shake");
+  let congratsImg = new Array(
+    "images/congrats.png",
+    "images/dog.png",
+    "images/bitwise.png"
+  );
+  let congratsRandomImg = Math.floor(Math.random() * congratsImg.length);
+  let congra = document.getElementById("congrats");
+  let logo = document.createElement("img");
+  congra.appendChild(logo);
+  logo.id = "congrats2";
+  document.getElementById("congrats2").src = congratsImg[congratsRandomImg];
+  congra.classList.add("shake");
   setTimeout(function () {
-    img.classList.remove("shake");
+    congra.classList.remove("shake");
   }, 3000);
 }
