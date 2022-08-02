e29302146480187815ecab3d7f980baf794307b8
Life90 <68822929+Life90@users.noreply.github.com>
2022-08-02 13:08:48 -0700

    final magic 8 ball  project

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
-document.querySelector("button").onclick = () => {
+function shake() {
+  let ball = document.getElementById("answers2");
+  if (ball != null) {
+    ball.parentNode.removeChild(ball);
+  }
+  answers.classList.add("shake");
+
+  setTimeout(function () {
+    answers.classList.remove("shake");
+  }, 1500);
+
+  setTimeout(function () {
+    getAnswers();
+  }, 0.003);
+}
+
+function getAnswers() {
   let magicBallsImg = new Array(
     "images/magic8ball_1.png",
     "images/magic8ball_3.png",
@@ -24,49 +40,46 @@ document.querySelector("button").onclick = () => {
     "images/magic8ball_start.png"
   );
 
-  let randomImg = Math.floor(Math.random() * magicBallsImg.length);
+  let randomImg =
+    magicBallsImg[Math.floor(Math.random() * magicBallsImg.length)];
   let answers = document.getElementById("answers");
-  let fortune = document.createElement("img");
-  answers.appendChild(fortune);
-  fortune.id = "answer2";
-  document.getElementById("answer2").src = magicBallsImg[randomImg];
-
-  answers.classList.add("shake");
-
-  // ball.classList.add("rotate");
-
-  setTimeout(function () {
-    answers.classList.remove("shake");
-  }, 1500);
-
-  // setTimeout(function () {
-  //   ball.classList.remove("rotate");
-  // }, 1500);
+  let newImg = document.createElement("img");
+  newImg.setAttribute("id", "answers2");
+  newImg.src = randomImg;
+  answers.appendChild(newImg);
 
   const input = document.querySelector("input").value;
   let disabled = document.getElementById("test");
-  if (input.length <= 3) {
+  if (input.length <= 5) {
     disabled = true;
     alert("Hey you need to enter your question first");
-  } else if (input.length > 3) {
+  } else if (input.length > 5) {
     disabled = false;
   }
-};
+}
 
 function showImg() {
+  let congra = document.getElementById("congrats");
+  let motivation = document.getElementById("congrats2");
+  if (motivation != null) {
+    motivation.parentNode.removeChild(motivation);
+  }
+
+  congra.classList.add("shake");
+
+  setTimeout(function () {
+    congra.classList.remove("shake");
+  }, 1500);
+
   let congratsImg = new Array(
     "images/congrats.png",
     "images/dog.png",
     "images/bitwise.png"
   );
-  let congratsRandomImg = Math.floor(Math.random() * congratsImg.length);
-  let congra = document.getElementById("congrats");
-  let logo = document.createElement("img");
-  congra.appendChild(logo);
-  logo.id = "congrats2";
-  document.getElementById("congrats2").src = congratsImg[congratsRandomImg];
-  congra.classList.add("shake");
-  setTimeout(function () {
-    congra.classList.remove("shake");
-  }, 3000);
+  let congratsRandomImg =
+    congratsImg[Math.floor(Math.random() * congratsImg.length)];
+  let bonas = document.createElement("img");
+  congra.appendChild(bonas);
+  bonas.setAttribute("id", "congrats2");
+  bonas.src = congratsRandomImg;
 }
